8d78c9d00b9f7de83132e8192d0e4c43364191a7
devangmalhotra <malh2226@mylaurier.ca>
2024-11-19 01:04:10 -0500

    finished responsive navbar

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-let menubarList = document.getElementById("menubar-list");
+let menuItems = document.getElementById("menu-items");
 let hamburger = document.getElementById("hamburger");
+hamburger.onclick = openCloseMenu;
+
 function myFunction(x) {
     if (x.matches) {
-        menubarList.classList.add("hidden");
+        menuItems.classList.add("active");
         hamburger.classList.remove("hidden");
     } else {
-        menubarList.classList.remove("hidden");
+        menuItems.classList.remove("active");
         hamburger.classList.add("hidden");
     }
+}
+
+function openCloseMenu() {
+    menuItems.classList.toggle("open");
 }
 
 let x = window.matchMedia("(max-width: 1000px)");
